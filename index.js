b02f9f458af1595bdd78d0f8f2ef9a8f2a4661db
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-23 18:10:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+//var otherDeli = ["Steven", "Blake", "Avi"];
+
+function takeANumber(katzDeliLine,name){
+    katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+
+
+// var katzDeliLine = ['Ada','grace','Kent']
+function nowServing(katzDeliLine){
+  //var katzDeliLine = ['Ada','Grace','Kent'];
+
+    if(katzDeliLine.length > 0){
+          return 'Currently serving ' + katzDeliLine.shift() + '.';
+
+}else{
+          return"There is nobody waiting to be served!";
+}
+}
+
+
+
+function currentLine(katzDeliLine){
+    if(katzDeliLine.length > 0){
+      for(var n = 0; n < katzDeliLine.length; n++)
+    var outPut = katzDeliLine.slice(0,-2);
+          return 'The line is currently:' + ' ' + '1. ' +outPut + ' ' + '2. ' + outPut + ' ' + '3. ' + outPut;
+    }else{
+          return 'The line is currently empty.';
+    }
+}
